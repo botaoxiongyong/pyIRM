31706ccaa9aa2eb64d6eff6119dd62943a136d64
Jiabo <kabol.liu@gmail.com>
2018-06-21 14:03:00 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+NAME = "pyIRM"
+
+setup(
+    name=NAME,
+    version='0.0.1',
+    author='Liu Jiabo',
+    author_email='kabol.liu@gmail.com',
+    packages=['pyIRM']
+)
